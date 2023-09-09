4ec311b0e4425172efe79fe7b8c222d563227e15
duperpara <duperpara@gmail.com>
2023-09-09 13:02:08 -0300

    fix package typing

--- src/auto_control_tools/controller/controller.py
+++ src/auto_control_tools/controller/controller.py
@@ -1,8 +1,17 @@
+from typing import Union, Dict, Any
+
 from ..model.model import Model
 
 
 class Controller:
-    def __init__(self, model: Model, ki: float = 0, kp: float = 0, kd: float = 0, meta: dict = None):
+    def __init__(
+            self,
+            model: Model,
+            ki: float = 0,
+            kp: float = 0,
+            kd: float = 0,
+            meta: Union[Dict[str, Any], None] = None
+    ):
         self.model = model
         self.view = ControllerView(self)
         self.meta = meta if meta else {}
@@ -18,7 +27,7 @@ class ControllerView:
     def plot_controller_graph(self, *args, **kwargs):
         pass
 
-    def get_controller_data(self) -> dict:
+    def get_controller_data(self) -> Dict[str, Any]:
         pass
 
     def print_controller_data(self, *args, **kwargs):
--- src/auto_control_tools/model/model.py
+++ src/auto_control_tools/model/model.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List, Dict, Any
 
 import control
 import pandas as pd
@@ -7,7 +7,11 @@ from ..utils.data_input import DataInputUtils
 
 
 class Model:
-    def __init__(self, tf: Union[control.TransferFunction, list], meta: dict = None):
+    def __init__(
+            self,
+            tf: Union[control.TransferFunction, List[List[float]]],
+            meta: Union[Dict[str, Any], None] = None
+    ):
         self.view = ModelView(self)
         if isinstance(tf, control.TransferFunction):
             self.system = tf
@@ -20,7 +24,7 @@ class Model:
         self.source_data = self.meta.get('source_data', pd.DataFrame(columns=DataInputUtils.standard_fields))
 
     def identify_model_order(self) -> int:
-        return self.tf.den[0][0].size
+        return int(self.tf.den[0][0].size)
 
 
 class ModelView:
@@ -30,9 +34,8 @@ class ModelView:
     def plot_model_graph(self, *args, **kwargs):
         pass
 
-    def get_model_data(self) -> dict:
+    def get_model_data(self) -> Dict[str, Any]:
         pass
 
     def print_model_data(self, *args, **kwargs):
         pass
-
