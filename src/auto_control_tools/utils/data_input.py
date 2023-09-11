61b55ba8fe694f8a06fc879fb491e7cb4527ce60
duper <duperpara@gmail.com>
2023-09-11 19:03:57 -0300

    add create_table_with_fields and read_table_with_fields plust tests

--- src/auto_control_tools/utils/data_input.py
+++ src/auto_control_tools/utils/data_input.py
@@ -1,5 +1,5 @@
 import os
-from typing import List
+from typing import List, Union
 
 import pandas as pd
 
@@ -15,7 +15,7 @@ class DataInputUtils:
             fields: List[str],
             table_name: str = 'data_input',
             save_as: str = 'csv'
-    ):
+    ) -> str:
         if save_as not in cls.allowed_file_type:
             raise ValueError(f'{save_as} is not an allowed file type')
 
@@ -23,12 +23,26 @@ class DataInputUtils:
         df = pd.DataFrame(columns=fields)
 
         if save_as == 'csv':
-            df.to_csv(file_path)
+            df.to_csv(file_path, index=False)
         elif save_as == 'xlsx':
-            df.to_excel(file_path)
+            df.to_excel(file_path, index=False)
         else:
             raise ValueError(f'{save_as} is not an allowed file type')
 
+        return file_path
+
     @classmethod
-    def read_table_with_fields(cls, path: str, fields: List[str]):
-        pass
+    def read_table_with_fields(cls, path: str, fields: Union[List[str]] = None) -> pd.DataFrame:
+        if fields is None:
+            fields = cls.standard_fields
+
+        if path.lower().endswith(".xlsx"):
+            df = pd.read_excel(path)
+        elif path.lower().endswith(".csv"):
+            df = pd.read_csv(path)
+        else:
+            raise ValueError(f'{os.path.splitext(path)[-1]} is not an allowed file type')
+
+        df = df[fields]
+
+        return df
