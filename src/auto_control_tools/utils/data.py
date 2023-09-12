1b9a209ca71570244921d065752c327357af8a94
duper <duperpara@gmail.com>
2023-09-12 02:20:18 -0300

    add pprint to data utils

--- src/auto_control_tools/utils/data.py
+++ src/auto_control_tools/utils/data.py
@@ -1,4 +1,20 @@
+from typing import Dict, Any
+
+import pandas as pd
+import pprint
+
+from auto_control_tools.utils.envoirment import is_jupyter_environment
+from IPython import display
+
+
 class DataUtils:
+    jupyter_env = is_jupyter_environment()
+
     @classmethod
-    def pprint_dict(cls):
-        pass
+    def pprint_dict(cls, di: Dict[str, Any]):
+        if cls.jupyter_env:
+            df = pd.DataFrame([di])
+            display.display(df)
+        else:
+            pprint.pprint(di)
+
