b536d640131bfca127e01c499488ecf12b857e36
duper <duperpara@gmail.com>
2023-09-12 01:15:13 -0300

    improve model.py

--- src/auto_control_tools/model/model.py
+++ src/auto_control_tools/model/model.py
@@ -2,6 +2,7 @@ from typing import Union, List, Dict, Any
 
 import control
 import pandas as pd
+import sympy as sp
 
 from ..utils.data_input import DataInputUtils
 
@@ -11,24 +12,38 @@ class Model:
             self,
             tf: Union[control.TransferFunction, List[List[float]]],
             order: Union[int, None] = None,
-            source_data: Union[pd.DataFrame, None] = None
+            source_data: Union[pd.DataFrame, None] = None,
+            num: Union[List[float], None] = None,
+            den: Union[List[float], None] = None,
     ):
-        self.system = self.get_control_system(tf)
+        if isinstance(tf, control.TransferFunction):
+            self.tf = tf
+            self.num = self.tf.num[0][0].tolist()
+            self.den = self.tf.den[0][0].tolist()
+        else:
+            self.tf = control.TransferFunction(*tf)
+            self.num = tf[0]
+            self.den = tf[1]
+
+        if num is not None:
+            self.num = num
+
+        if den is not None:
+            self.den = den
+
+        s = sp.symbols('s')
+        num_symbolic = sum([coefficient * s ** i for i, coefficient in enumerate(reversed(self.num))])
+        den_symbolic = sum([coefficient * s ** i for i, coefficient in enumerate(reversed(self.den))])
+        self.tf_symbolic = num_symbolic / den_symbolic
+
         self.order = order if order is not None else self.identify_model_order()
         self.source_data = source_data if source_data is not None else pd.DataFrame(
             columns=DataInputUtils.standard_fields)
 
         self.view = ModelView(self)
 
-    @staticmethod
-    def get_control_system(tf: Union[control.TransferFunction, List[List[float]]]):
-        if isinstance(tf, control.TransferFunction):
-            return tf
-        else:
-            return control.TransferFunction(*tf)
-
     def identify_model_order(self) -> int:
-        return int(self.system.den[0][0].size) - 1
+        return int(self.tf.den[0][0].size) - 1
 
 
 class ModelView:
