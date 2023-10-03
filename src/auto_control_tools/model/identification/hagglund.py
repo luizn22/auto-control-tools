643e7deb5405f3b2c4a7edf72342b03b6449da5d
duper <duperpara@gmail.com>
2023-10-03 06:12:23 -0300

    add hagglund.py identification method

---
+++ src/auto_control_tools/model/identification/hagglund.py
@@ -0,0 +1,34 @@
+from typing import Union
+
+from ..first_order_model import FirstOrderModel
+from .base import BaseModelIdentification
+
+
+class HagglundModelIdentification(BaseModelIdentification):
+    @classmethod
+    def get_model(
+            cls,
+            path: str,
+            sample_time: Union[None, float] = None,
+            step_signal: Union[None, float] = None,
+            ignore_delay_threshold: float = 0.5,
+
+    ) -> FirstOrderModel:
+        df = cls.get_model_data_default(path, sample_time, step_signal)
+        tf_data, step_signal = cls.setup_data_default(df, sample_time, step_signal)
+
+        idx_vreg, vreg = cls.get_vreg(tf_data)
+        idx_tan, tan = cls.get_max_tan(tf_data)
+        tan_point_value = tf_data.loc[tf_data.index == idx_tan].iloc[0]
+
+        t1 = cls.get_time_from_inclination(idx_tan, tan_point_value, tan_point_value, 0)
+        t2 = tf_data[tf_data == min(tf_data[tf_data >= vreg*0.632])].index[0]
+
+        K = vreg / step_signal
+        tau = t2 - t1
+        teta = t1
+
+        if teta < ignore_delay_threshold:
+            teta = 0
+
+        return FirstOrderModel(K, tau, teta, source_data=tf_data)
