7ebb7ec0d0c1e8b1efba9be6a049e614da9c5a36
duperpara <duperpara@gmail.com>
2023-10-22 21:56:30 -0300

    add identification Classes to model\__init__.py

--- src/auto_control_tools/model/__init__.py
+++ src/auto_control_tools/model/__init__.py
@@ -1,3 +1,7 @@
 from .model import Model  # noqa
 from .first_order_model import FirstOrderModel  # noqa
 from .identification.ziegler_nichols import ZieglerNicholsModelIdentification # noqa
+from .identification.hagglund import HagglundModelIdentification # noqa
+from .identification.nishikawa import NishikawaModelIdentification # noqa
+from .identification.smith import SmithModelIdentification # noqa
+from .identification.sundaresan_krishnaswamy import SundaresanKrishnaswamyModelIdentification # noqa
