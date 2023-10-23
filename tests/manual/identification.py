25c9b42fb2a06678326499e43f92ec03747d1002
duperpara <duperpara@gmail.com>
2023-10-22 21:56:42 -0300

    update manual tests

--- tests/manual/identification.py
+++ tests/manual/identification.py
@@ -4,7 +4,7 @@ import os
 def run():
     import auto_control_tools as act
     pth = r'C:\Users\luiz\PycharmProjects\auto-control-tools\tests\manual'
-    m = act.ZieglerNicholsModelIdentification.get_model(os.path.join(pth, 'data_input.csv'))
+    m = act.NishikawaModelIdentification.get_model(os.path.join(pth, 'data_input.csv'))
     m.view.plot_model_graph()
     m.view.print_model_data()
 
