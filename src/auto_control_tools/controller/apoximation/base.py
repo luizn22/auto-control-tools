a4dfb9e92ad2a92d5a47ad96d78d87f7a9b90c08
duper <duperpara@gmail.com>
2023-10-23 22:40:22 -0300

    add controller_type to base controller

--- src/auto_control_tools/controller/apoximation/base.py
+++ src/auto_control_tools/controller/apoximation/base.py
@@ -3,8 +3,23 @@ from abc import abstractmethod
 from ...model.model import Model
 from ...controller.controller import Controller
 
+P = 'P'
+I = 'I'  # noqa
+D = 'D'
+PI = P+I
+PD = P+D
+PID = P+I+D
+
 
 class BaseControllerAproximation:
+    accepted_controllers = [P, I, D, PI, PD, PID]
+
+    @classmethod
     @abstractmethod
-    def get_controller(self, model: Model, *args, **kwargs) -> Controller:
-        pass
+    def get_controller(cls, model: Model, controller_type: str) -> Controller:
+        raise NotImplementedError('get_controller must be implemented in a subclass')
+
+    @classmethod
+    def _parse_controller_option(cls, controller_type: str):
+        if controller_type not in cls.accepted_controllers:
+            raise ValueError(f'Invalid Controller option, must be one of {cls.accepted_controllers}')
