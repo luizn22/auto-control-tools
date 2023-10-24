fa18c84ef2b6a8ac6d706a04cf86f3d295f2c141
duper <duperpara@gmail.com>
2023-10-23 22:47:26 -0300

    add PID options, ZieglerNicholsControllerAproximation and CohenCoonControllerAproximation to controller\__init__.py

--- src/auto_control_tools/controller/__init__.py
+++ src/auto_control_tools/controller/__init__.py
@@ -1,1 +1,4 @@
 from .controller import Controller  # noqa
+from .apoximation.base import P, I, D, PI, PD, PID  # noqa
+from .apoximation.ziegler_nichols import ZieglerNicholsControllerAproximation  # noqa
+from .apoximation.cohen_coon import CohenCoonControllerAproximation  # noqa
