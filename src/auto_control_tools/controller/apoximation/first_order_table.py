67fda934423bf3b02d790a1ba8d23ced69f0b86b
duper <duperpara@gmail.com>
2023-10-23 22:45:45 -0300

    add FirstOrderTableControllerAproximation as template for table aproximations

---
+++ src/auto_control_tools/controller/apoximation/first_order_table.py
@@ -0,0 +1,28 @@
+from abc import abstractmethod
+from typing import Dict
+
+from .base import BaseControllerAproximation
+from ...model.first_order_model import FirstOrderModel
+from ...controller.controller import Controller
+
+
+class FirstOrderTableControllerAproximationItem:
+    controller_type: str = ''  # replace with controller type
+
+    @classmethod
+    @abstractmethod
+    def get_controller(cls, model: FirstOrderModel) -> Controller:
+        pass
+
+
+class FirstOrderTableControllerAproximation(BaseControllerAproximation):
+    controller_table: Dict[str, FirstOrderTableControllerAproximationItem] = {}
+
+    @classmethod
+    def get_controller(
+            cls,
+            model: FirstOrderModel,  # type: ignore[override]
+            controller_type: str
+    ) -> Controller:
+        cls._parse_controller_option(controller_type)
+        return cls.controller_table[controller_type].get_controller(model)
