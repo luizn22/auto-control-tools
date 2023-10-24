142402fa48b9d46fd578753a5fbc3271665b3013
duper <duperpara@gmail.com>
2023-10-23 22:41:24 -0300

    refactor Controller feedback calculations

--- src/auto_control_tools/controller/controller.py
+++ src/auto_control_tools/controller/controller.py
@@ -23,12 +23,18 @@ class Controller:
 
         s = sp.symbols('s')
         Kp, Ki, Kd = sp.symbols('Kp Ki Kd')
-        pid_symbolic = Kp + (Ki / s) + (Kd * s)
+        pid_symbolic = kp + (ki / s) + (kd * s)
 
         self.tf_symbolic = model.tf_symbolic * pid_symbolic / (1 + model.tf_symbolic * pid_symbolic)
+        self.tf_symbolic = sp.simplify(sp.trigsimp(sp.nsimplify(sp.trigsimp(self.tf_symbolic, tolerance=1e-4, rational=True))))
 
-        pid = control.TransferFunction([kd, kp, ki], [1])
-        self.tf = model.tf.feedback(pid)
+        s = control.TransferFunction.s
+        pid = kd * s + kp + ki/s
+        # pid = control.TransferFunction([kd, kp, ki], [1])
+        self.tf = (model.tf * pid) / (1 + model.tf * pid)
+        if (self.tf.num[0][0][-1] == 0) and (self.tf.den[0][0][-1] == 0):
+            self.tf = control.TransferFunction(self.tf.num[0][0][:-1], self.tf.den[0][0][:-1])
+        # self.tf = model.tf.feedback(pid)
 
         self.view = ControllerView(self)
 
