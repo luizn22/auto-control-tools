4aea3214ab8c3270b247af477e46be3e38ac445b
duper <duperpara@gmail.com>
2023-10-23 22:45:08 -0300

    remove pade application from first_order_model.py - it is now applied on plot

--- src/auto_control_tools/model/first_order_model.py
+++ src/auto_control_tools/model/first_order_model.py
@@ -17,7 +17,7 @@ class FirstOrderModel(Model):
             K: float,
             tau: float,
             teta: float = 0,
-            pade_degree: int = 1,
+            pade_degree: int = 5,
             source_data: Union[pd.Series, None] = None,
     ):
         super().__init__([[K], [tau, 1]], source_data=source_data)
@@ -25,9 +25,8 @@ class FirstOrderModel(Model):
         self.tau = tau
         self.teta = teta
 
-        if teta != 0:
+        if teta != 0 and pade_degree > 0:
             self.pade = control.tf(*control.pade(teta, pade_degree))
-            self.tf = self.tf * self.pade
 
             s = sp.symbols('s')
             self.tf_symbolic = self.tf_symbolic * sp.exp(-teta*s)
