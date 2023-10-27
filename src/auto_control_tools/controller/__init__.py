3e01d67fcd7d57cc0d526596f06c5d5b03d17b58
duper <duperpara@gmail.com>
2023-10-27 19:45:45 -0300

    add ControllerView to controller __init__.py

--- src/auto_control_tools/controller/__init__.py
+++ src/auto_control_tools/controller/__init__.py
@@ -1,10 +1,10 @@
-from .controller import Controller  # noqa
+from .controller import Controller, ControllerView  # noqa
 from .apoximation.base import P, I, D, PI, PD, PID  # noqa
 from .apoximation.ziegler_nichols import ZieglerNicholsControllerAproximation  # noqa
 from .apoximation.cohen_coon import CohenCoonControllerAproximation  # noqa
 
 __all__ = [
-    "Controller",
+    "Controller", "ControllerView",
     "P", "I", "D", "PI", "PD", "PID",
     "ZieglerNicholsControllerAproximation",
     "CohenCoonControllerAproximation",
