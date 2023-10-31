26d5c32c7a5d088a4a54b00da9f29f2e1119eabc
duper <duperpara@gmail.com>
2023-10-31 01:17:13 -0300

    rollback manual test import_package.py

--- tests/manual/import_package.py
+++ tests/manual/import_package.py
@@ -1,18 +1,6 @@
-from tests.conftest import FIRST_ORDER_IDENTIFICATION_TEST_CASES
-
-
-def run():
-    import auto_control_tools as act
-    x = FIRST_ORDER_IDENTIFICATION_TEST_CASES[2].copy()
-
-    m = act.NishikawaModelIdentification.get_model(**x)  # type: ignore
-
-    c = act.CohenCoonControllerAproximation.get_controller(m, act.PID)
-
-    c.view.print_tf()
-    c.view.print_model_step_response_data()
-    c.view.plot_model_step_response_graph()
+import auto_control_tools
 
 
 if __name__ == '__main__':
-    run()
+    m = auto_control_tools.Model(([1], [1, 2, 3]))
+    print('aaa')
