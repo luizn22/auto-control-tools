bc636dccae9d4f043bc2d029abed2a339fdc16a4
duper <duperpara@gmail.com>
2023-11-21 05:29:59 -0300

    update manual test

--- tests/manual/bib_comp.py
+++ tests/manual/bib_comp.py
@@ -1,19 +1,53 @@
+import os
+
 import auto_control_tools as act
 
-model = act.FirstOrderModel(K=1.95, tau=8.33, theta=1.48)
+folders = [
+    r'../resources/forno/',
+    r'../resources/mesa_giratoria/',
+    r'../resources/nivel_tanque/',
+]
 
-model.view.print_tf()
-model.view.print_model_step_response_data()
-model.view.plot_model_step_response_graph()
+di = {}
 
-controller = act.ZieglerNicholsControllerAproximation.get_controller(model, act.PID)
+for folder in folders:
+    di[folder] = {}
+    for file in os.listdir(folder):
+        di[folder][file] = {}
+        file_path = os.path.join(folder, file)
+        print(file_path)
+        last = 'none'
+        try:
+            di[folder][file]['model'] = {}
+            last = 'model'
 
-controller.view.print_tf()
-controller.view.print_controller_step_response_data()
-controller.view.plot_controller_step_response_graph(plot_model=False)
+            model = act.SmithModelIdentification.get_model(
+                file_path,
+                sample_time=1,
+                ignore_delay_threshold=0,
+                use_lin_filter=True,
+                # linfilter_smoothness=5
+            )
+            di[folder][file]['model']['tf'] = str(model.tf_symbolic)
+            di[folder][file]['model']['resp'] = model.view.get_model_step_response_data()
 
-controller = act.CohenCoonControllerAproximation.get_controller(model, act.PID)
+            model.view.print_tf()
+            model.view.print_model_step_response_data()
+            # model.view.plot_model_step_response_graph()
 
-controller.view.print_tf()
-controller.view.print_controller_step_response_data()
-controller.view.plot_controller_step_response_graph(plot_model=False)
+            di[folder][file]['controller'] = {}
+
+            last = 'controller'
+            controller = act.CohenCoonControllerAproximation.get_controller(model, act.PID)
+            di[folder][file]['controller']['tf'] = str(controller.tf_symbolic)
+            di[folder][file]['controller']['resp'] = controller.view.get_controller_step_response_data()
+
+            controller.view.print_tf()
+            controller.view.print_controller_step_response_data()
+            # controller.view.plot_controller_step_response_graph(plot_model=False)
+
+        except Exception as e:
+            di[folder][file][last]['error'] = str(e)
+            print(e)
+
+print(di)
