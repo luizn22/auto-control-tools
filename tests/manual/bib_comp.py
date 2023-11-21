a1fd6dae9ae9a420754fa6de484f3698f43c1434
duper <duperpara@gmail.com>
2023-11-21 02:02:51 -0300

    add manual test

---
+++ tests/manual/bib_comp.py
@@ -0,0 +1,19 @@
+import auto_control_tools as act
+
+model = act.FirstOrderModel(K=1.95, tau=8.33, theta=1.48)
+
+model.view.print_tf()
+model.view.print_model_step_response_data()
+model.view.plot_model_step_response_graph()
+
+controller = act.ZieglerNicholsControllerAproximation.get_controller(model, act.PID)
+
+controller.view.print_tf()
+controller.view.print_controller_step_response_data()
+controller.view.plot_controller_step_response_graph()
+
+controller = act.CohenCoonControllerAproximation.get_controller(model, act.PID)
+
+controller.view.print_tf()
+controller.view.print_controller_step_response_data()
+controller.view.plot_controller_step_response_graph()
